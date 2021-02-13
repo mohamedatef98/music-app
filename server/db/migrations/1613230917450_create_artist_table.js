2d49c0b7cf1bfd8933770b1a86b19d94480443c5
Mohamed Atef <eng.mohamedatef98@gmail.com>
2021-02-13 18:02:27 +0200

    Add Artist Model

---
+++ server/db/migrations/1613230917450_create_artist_table.js
@@ -0,0 +1,9 @@
+module.exports = {
+    up: `CREATE TABLE Artist (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name varchar(40) NOT NULL,
+        imageFileId INT,
+        FOREIGN KEY (imageFileId) REFERENCES ImageFile(fileId)
+    )`,
+    down: 'DROP TABLE Artist;'
+}
---
+++ server/db/models/Artist.js
@@ -0,0 +1,34 @@
+const Joi = require('joi')
+const { query } = require('../db_connection')
+
+const schema = Joi.object({
+  name: Joi.string()
+    .required()
+    .alphanum()
+    .min(5)
+    .max(30),
+
+  imageFileId: [
+    Joi.string().equal('NULL'),
+    Joi.number()
+  ]
+})
+
+module.exports = {
+  async create({ name, imageFileId='NULL' }) {
+    schema.validate({ name, imageFileId })
+
+    const [results] = await query('INSERT INTO Artist (name, imageFileId) values(?, ?);', [name, imageFileId])
+    const [[artist]] = await query('SELECT id, name, imageFileId FROM Artist WHERE id = ?', [results.insertId])
+    return artist
+  },
+
+  async getAll() {
+    const [artists] = await query('SELECT id, name, imageFileId FROM Artist;', [])
+    return artists
+  },
+  async getById(id) {
+    const [[artist]] = await query('SELECT id, name, imageFileId FROM Artist WHERE id = ?;', [id])
+    return artist
+  }
+}
