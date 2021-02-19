57afe012b4af4fc38d83c3f2871c538b8b7e463b
Mohamed Atef <eng.mohamedatef98@gmail.com>
2021-02-19 14:58:12 +0200

    Remove api folder

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,11 +5,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const ctx = useContext(Constants)
-  console.log(ctx)
+
   return (
     <Layout>
       <section>
-        <h2 onClick={() => ctx.changeTheme('light')}>Artists</h2>
+        <h2>Artists</h2>
       </section>
     </Layout>
   )
